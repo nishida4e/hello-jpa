fa3c1199f0ad76cb8ce1433b103c477bd115356b
nishida4e <s_nishida@four-e.co.jp>
2016-12-15 20:18:17 +0900

    jpa

---
+++ src/main/java/hello/MessageService.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+/**
+ *
+ * @author nishida
+ */
+@ApplicationScoped
+
+public class MessageService {
+    
+}
